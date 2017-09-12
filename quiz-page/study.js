7a55dd5ccf3b267c1befbc48198f4f2bfee841d9
Leo McClung <murraythemoose@gmail.com>
2017-09-12 15:43:49 -0600

    added three info colums, header, footer, and quiz button

--- quiz-page/study.js
+++ quiz-page/study.js
@@ -4,7 +4,7 @@ function randomInteger(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-  //    make sure max is numberOfItems-1 to ensure a valid array index
+//    make sure max is numberOfItems-1 to ensure a valid array index
 
 
 // Fetch items from the NASA images API
@@ -13,11 +13,19 @@ fetch('https://images-api.nasa.gov/search?keywords=galaxies&description=galaxy&c
   .then(response => response.json())
   .then(body => {
     var numberOfItems = body.collection.items.length;
-    var randomIndexItem = randomInteger(numberOfItems-1);
-    var randomItem = body.collection.length[randomIndexItem];
+    var randomIndexItem = randomInteger(0, numberOfItems-1);
+    var randomItem = body.collection.items[randomIndexItem];
+    var randomImg = randomItem.links[0];
+
+    document.body.style.backgroundImage = 'url("' + randomImg.href + '")';
+    document.body.style.backgroundAttachment = 'fixed';
+    
+    
+    // document.body.style.background = 'url("' + randomImg.href + '")';
+
     
     // Pull the thumbnail source from the item.links[0].href field
     // Update the document.body style background to the image url obtained above
-
+    // console.log(randomImg)
   })
   .catch(e => console.error(e));
