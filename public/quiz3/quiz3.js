5e8a28613d841f0d784c0d227fa502739d195505
Leo McClung <murraythemoose@gmail.com>
2017-09-14 17:01:56 -0600

    minor text edits

--- public/quiz3/quiz3.js
+++ public/quiz3/quiz3.js
@@ -2,6 +2,38 @@ $(document).ready(function() {
     $('select').material_select();
   });
 
+  // Function to generate a random integer between min & max (inclusive)
+function randomInteger(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//    make sure max is numberOfItems-1 to ensure a valid array index
+
+
+// Fetch items from the NASA images API
+
+fetch('https://images-api.nasa.gov/search?keywords=galaxies&description=galaxy&center=jpl&media_type=image')
+  .then(response => response.json())
+  .then(body => {
+    var numberOfItems = body.collection.items.length;
+    var randomIndexItem = randomInteger(0, numberOfItems-1);
+    var randomItem = body.collection.items[randomIndexItem];
+    var randomImg = randomItem.links[0];
+
+    document.body.style.backgroundImage = 'url("' + randomImg.href + '")';
+    document.body.style.backgroundAttachment = 'fixed';
+    
+    
+    // document.body.style.background = 'url("' + randomImg.href + '")';
+
+    
+    // Pull the thumbnail source from the item.links[0].href field
+    // Update the document.body style background to the image url obtained above
+    // console.log(randomImg)
+  })
+  .catch(e => console.error(e));
+
 
 function countDown(i) {
     var int = setInterval(function () {
